2e42f636b77af447839497a8e4d9806e8b644104
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-03-30 22:28:46 +0300

    finished adaptive design

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,7 @@ $(function () {
         $('.menu').toggleClass('menu--open');
     });
 
-    if ($(window).width()<651) {
+    if ($(window).width() < 651) {
         $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
     }
 
@@ -25,7 +25,51 @@ $(function () {
         slidesToShow: 10,
         slidesToScroll: 10,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1751,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 541,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
